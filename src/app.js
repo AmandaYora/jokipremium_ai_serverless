92099381000a4b18474783bdeccb79d7a8be4a1b
Dimas Prasetio <dimas.prasetio@neuronworks.co.id>
2025-10-30 15:42:45 +0700

    ignore cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,24 @@ import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
+const DEFAULT_ALLOWED_HEADERS =
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization";
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    req.headers["access-control-request-headers"] || DEFAULT_ALLOWED_HEADERS
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
